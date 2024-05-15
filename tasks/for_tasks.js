ac0db6c27efaafcec17c3266c571b1f1e25251e4
vkarpovich-sc <vkarpovich@scand.com>
2024-05-15 14:49:25 +0300

    updated tasks

--- tasks/for_tasks.js
+++ tasks/for_tasks.js
@@ -18,11 +18,64 @@ function reverseNumber(number) {
   return reversed;
 }
 
-const originalNumber = 12345; 
+const originalNumber = 12345;
 const reversedNumber = reverseNumber(originalNumber);
 
 console.log(
   `Число, получаемое при прочтении цифр ${originalNumber} справа налево: ${reversedNumber}`
 );
 
-/*Напечатать таблицу перевода 1, 2, ... 20 долларов США в рубли по текущему курсу */
+/*Напечатать таблицу перевода 1, 2, ... 20 долларов США
+ в рубли по текущему курсу */
+const currency = async () => {
+  const currency_table = await fetch(`https://open.er-api.com/v6/latest/USD`)
+    .then((res) => res.json())
+    .then((data) => {
+      return data.rates.BYN;
+    });
+  console.log(currency_table, typeof currency_table);
+
+  return Number(currency_table);
+};
+
+(async function () {
+  const byn_currency = await currency();
+  console.log(typeof byn_currency);
+  const container = document.getElementById("container");
+  const table = document.createElement("Table");
+  table.innerHTML = `<tr>
+    <th>USD</th>
+    <th>BYN</th>
+    </tr>`;
+  container.appendChild(table);
+  for (let i = 0; i < 21; i++) {
+    table.innerHTML += `<tr>
+      <th>${i}</th>
+      <th>${i * byn_currency}</th>
+      </tr>`;
+  }
+})();
+
+/*Напечатать таблицу Пифагора */
+
+(function () {
+  const container = document.getElementById("container");
+  const table = document.createElement("Table");
+  table.innerHTML = `<tr id="row">
+    <th>X</th>
+    </tr>`;
+  container.appendChild(table);
+  for (let i = 1; i < 11; i++) {
+    let row = document.getElementById("row");
+    row.innerHTML += `<th>${i}</th>`;
+    table.innerHTML += `<tr id="column${i}"> </tr>`;
+
+    for (let j = 0; j < 11; j++) {
+      let jCopy = j;
+      let column = document.getElementById(`column${i}`);
+      j == 0
+        ? (column.innerHTML += `<th>${(1 + jCopy) * i}</th>`)
+        : (column.innerHTML += `<th>${j * i}</th>`);
+    }
+  }
+})();
