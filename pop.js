c8899828085ed0ca5c4f7f3aa338e86170e0d7ae
vkarpovich-sc <vkarpovich@scand.com>
2024-05-20 16:48:24 +0300

    add functions for floors and residents generation

--- pop.js
+++ pop.js
@@ -1,10 +1,24 @@
+let residents = 100;
+let arrivedResidents = 0;
+
+/*const generateAmount = (amount) => {
+  let arr = []
+  for ( let i =0; i < amount; i++) {
+    arr.push(this.floorNumber, Math.floor(Math.random() * 9))
+  }
+  return peopleOnFloor
+}*/
+
 class Person {
   constructor(startPoint, endPoint) {
     this.startPoint = startPoint;
     this.endPoint = endPoint;
   }
 
-  checkDirection() {}
+  checkDirection(elEndPoint) {
+    // проверяем, стоит ли жителю заходить в лифт
+    this.startPoint < elEndPoint ? false : true;
+  }
 }
 
 class Floor {
@@ -12,14 +26,25 @@ class Floor {
     this.floorNumber = floorNumber;
     this.peopleAmount = peopleAmount;
   }
-  generatePeople() {}
+  generatePeople() {
+    let peopleOnFloor = []; // массив с жителями этажа
+    for (let i = 0; i < this.peopleAmount; i++) {
+      peopleOnFloor.push(
+        new Person(this.floorNumber, Math.floor(Math.random() * 9))
+      );
+    }
+    console.log(
+      `людей на этаже ${this.floorNumber} -- ${peopleOnFloor.length}`
+    );
+    return peopleOnFloor;
+  }
   checkPersonFloor() {}
   setArrived() {}
   removeWaiting() {}
 }
 
 class Elevator {
-  constructor(elStartPoint, elEndPoint, capacity) {
+  constructor(elStartPoint, capacity) {
     this.elStartPoint = elStartPoint;
     this.elEndPoint = elEndPoint;
     this.capacity = capacity;
@@ -27,7 +52,9 @@ class Elevator {
 
   checkCapacity() {}
   goTo() {}
-  checkDirection() {}
+  checkDirection() {
+
+  }
   updateNumberOfPassengers() {}
 }
 
@@ -37,7 +64,30 @@ class Building {
     this.peopleAmount = peopleAmount;
   }
 
-  generateFloors () {}
-  startProcess () {}
+  generateFloors() {
+    let floors = [];
+    for (let i = 0; i < this.numberOfFloors; i++) {
+      let residentsPerFloor = Math.floor(Math.random() * residents);
+      residents = residents - residentsPerFloor; // уменьшаем возможное перенаселение офиса
+      floors.push(new Floor(i, residentsPerFloor));
+      floors[i].generatePeople(); // создаем жителей этажа
+    }
+    return floors;
+  }
+  startProcess() {
+    let floors = this.generateFloors();
+    let startPoint = floors.find((el) => {
+      el.length > 1;
+    });
+    const startElevator = () => {
+      while (arrivedResidents < 100) {
+        const elevator = new Elevator(startPoint, 6);
+      }
+    };
+  }
+
   checkArrival() {}
 }
+
+const start = new Building(9, 100);
+start.generateFloors();
