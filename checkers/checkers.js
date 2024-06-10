6da1a8ed588d4d70b2b43aefc09224e6651828ec
vkarpovich-sc <vkarpovich@scand.com>
2024-06-10 16:54:50 +0300

    improving functionality

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -1,192 +1,146 @@
-const div = document.getElementById("container");
-const table = document.getElementsByTagName("table")[0];
-const letters = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`];
 class Checker {
-  constructor(posLetter, posNumber, color, isBot) {
+  // класс шашка. создает саму шашку, задает ее цвет и обозначает методы движения
+  constructor(posLetter, posNumber, color) {
     this.posLetter = posLetter;
     this.posNumber = posNumber;
-    this.pos = `${this.posNumber}-${this.posLetter}`;
     this.color = color;
-    this.isBot = isBot;
+    this.pos = `pos-${this.posNumber}-${this.posLetter}`;
   }
 
-  click() {
-    // alert(this.pos);
-    //stopPreparation()
+  geretateCheck() {}
+}
 
-    let number = this.posNumber + 1;
-    const SAMEID = document.getElementsByTagName(`th`);
-    let indexOfPosLetter = letters.indexOf(this.posLetter);
-    const rightLetter =
-      indexOfPosLetter + 1 <= 8 ? letters[indexOfPosLetter + 1] : null;
-    const leftLetter =
-      indexOfPosLetter - 1 >= 0 ? letters[indexOfPosLetter - 1] : null;
-
-    const yellowElements = Array.from(SAMEID).filter(
-      (element) => element.style.backgroundColor === "rgb(247, 220, 111)"
-    );
-
-    yellowElements.forEach((el) => {
-      el.style.backgroundColor = `rgb(23, 126, 211)`;
-    });
-
-    switch (true) {
-      case rightLetter == null:
-        let th1 = document.getElementById(`pos-${number}-${leftLetter}`);
-        if (th1.hasChildNodes()) {
-          return;
-        } else {
-          th1.style.backgroundColor = `rgb(247, 220, 111 )`;
-        }
-        break;
-      case leftLetter == null:
-        let th2 = document.getElementById(`pos-${number}-${leftLetter}`);
-        if (th2.hasChildNodes()) {
-          return;
-        } else {
-          th2.style.backgroundColor = `rgb(247, 220, 111 )`;
-        }
-        break;
-      default:
-        let leftTh = document.getElementById(`pos-${number}-${leftLetter}`);
-        let rightTh = document.getElementById(`pos-${number}-${rightLetter}`);
-        rightTh.addEventListener("click", () => {
-          let check = document.getElementById(`pos-${this.pos}`);
-          check.innerHTML = ``;
-          let k = document.createElement(`div`);
-          k.style.cssText = `background-color: ${this.color};
-          border: 2px solid gray;
-          border-radius: 50px;
-          width: 40px;
-          height:40px;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);`;
-          rightTh.append(k);
-          rightTh.style.backgroundColor = `rgb(23, 126, 211)`;
-          leftTh.style.backgroundColor = `rgb(23, 126, 211)`;
-        });
-        leftTh.addEventListener("click", () => {
-          let check = document.getElementById(`pos-${this.pos}`);
-          check.innerHTML = ``;
-          let k = document.createElement(`div`);
-          k.style.cssText = `background-color: ${this.color};
-          border: 2px solid gray;
-          border-radius: 50px;
-          width: 40px;
-          height:40px;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);`;
-          leftTh.append(k);
-          leftTh.style.backgroundColor = `rgb(23, 126, 211)`;
-          rightTh.style.backgroundColor = `rgb(23, 126, 211)`
-        });
-        if (
-          leftTh.hasChildNodes() ||
-          rightTh.hasChildNodes() ||
-          (leftTh.hasChildNodes() && rightTh.hasChildNodes())
-        ) {
-          return;
-        } else {
-          leftTh.style.backgroundColor = `rgb(247, 220, 111 )`;
-          rightTh.style.backgroundColor = `rgb(247, 220, 111 )`;
-        }
-    }
+class Simple extends Checker {
+  // класс простой шашки. здесь написано ее движение и цвет, а также принимает позицию.
+  constructor(posLetter, posNumber, color) {
+    super(posLetter, posNumber, color);
+    this.pos = `pos-${this.posNumber}-${this.posLetter}`;
   }
 
-  createCheck() {
-    let check;
-    if (this.isBot) {
-      check = document.createElement(`div`);
-      check.style.cssText = `background-color: ${this.color};
-      border: 2px solid gray;
-      border-radius: 50px;
-      width: 40px;
-      height:40px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);`;
-      check.addEventListener("click", () => {
-        this.click();
-      });
-    } else {
-      check = document.createElement(`div`);
-      check.style.cssText = `background-color: ${this.color};
-      border: 2px solid gray;
-      border-radius: 50px;
-      width: 40px;
-      height:40px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);`;
-      check.addEventListener("click", (e) => {
-        alert(this.pos);
-      });
-    }
-
+  generateCheck() {
+    const check = document.createElement(`div`);
+    check.style.cssText = `background-color: ${this.color};
+    border: 2px solid gray;
+    border-radius: 50px;
+    width: 40px;
+    height:40px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);`;
+    /*check.addEventListener(`click`, () => {
+      this.handleMove()
+    })*/
     return check;
   }
 }
-(function () {
-  let checks = [];
-  for (let i = 0; i < 8; i++) {
-    let tr = document.createElement("tr");
-    checks[i] = [];
-    for (let j = 0; j < 8; j++) {
-      if (i + 1 == 6 || i + 1 == 7 || i + 1 == 8) {
-        checks[i][j] = new Checker(letters[j], i + 1, `white`, false);
-      } else {
-        checks[i][j] = new Checker(letters[j], i + 1, `black`, true);
+
+class Lady extends Checker {
+  // здесь написаны движение дамки
+  constructor() {}
+}
+
+class Field {
+  // создание поля при помощи класса клетки
+  constructor() {}
+  generateField() {
+    //const div = document.getElementById(`container`);
+    const table = document.getElementsByTagName(`table`)[0];
+    const letters = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`];
+    const human = new Human();
+    const bot = new Bot();
+    let checks = [];
+    for (let i = 0; i < 8; i++) {
+      const tr = document.createElement(`tr`);
+      checks[i] = [];
+      for (let j = 0; j < 8; j++) {
+        let cellColor = (i + j) % 2 === 0 ? "white" : "black";
+        const cell = new Cell(cellColor, `pos-${i + 1}-${letters[j]}`);
+        const cellElement = cell.generateCell();
+        if (i == 3 || i == 4) {
+          tr.appendChild(cellElement);
+          continue;
+        }
+        if (i + 1 == 6 || i + 1 == 7 || i + 1 == 8) {
+          checks[i][j] = human.generateCheck(letters[j], i + 1);
+        } else {
+          checks[i][j] = bot.generateCheck(letters[j], i + 1);
+        }
+        if (cellColor == `black`) {
+          cellElement.append(checks[i][j]);
+        }
+        tr.append(cellElement);
       }
-
-      let th = document.createElement("th");
-
-      th.setAttribute("id", `pos-${i + 1}-${letters[j]}`);
-
-      // th.addEventListener("click" ,checks[i][j].click() )
-      if (i == 3 || i == 4) {
-        tr.appendChild(th);
-        continue;
-      } else if ((i + 1) % 2 && j % 2) {
-        th.append(checks[i][j].createCheck());
-      } else if (!((i + 1) % 2) && !(j % 2)) {
-        th.append(checks[i][j].createCheck());
-      }
-
-      tr.appendChild(th);
+      table.append(tr);
     }
-    table.appendChild(tr);
   }
+}
+
+class Player {
+  constructor() {}
+}
+
+class Human extends Player {
+  constructor() {
+    super()
+    this.check = [];
+  }
+  generateCheck(posL, posN) {
+    const check = new Simple(posL, posN, `white`).generateCheck();
+    check.addEventListener(`click`, () => {
+      alert(`${posL} + ${posN}`);
+    });
+    this.check.push(check);
+    return check;
+  }
+}
+
+class Bot extends Player {
+  constructor() {
+    super()
+    this.check = [];
+  }
+  generateCheck(posL, posN) {
+    const check = new Simple(posL, posN, `black`).generateCheck();
+    check.addEventListener(`click`, () => {
+      alert(`${posL} + ${posN} + BOT`);
+    });
+    this.check.push(check);
+    return check;
+  }
+}
+
+class Cell {
+  constructor(color, id) {
+    this.color = color;
+    this.id = id;
+  }
+
+  generateCell() {
+    const cell = document.createElement(`th`);
+    cell.setAttribute(`id`, this.id);
+    cell.style.cssText = `
+    border: 1px solid rgb(32, 159, 209);
+    width: 7%;
+    padding-top: 10%; 
+    position: relative;`;
+    return cell;
+  }
+}
+
+class Rules {
+  constructor() {}
+}
+
+class Game {
+  constructor() {}
+}
+
+class Application {
+  constructor() {}
+}
+
+(function () {
+  const field = new Field();
+  field.generateField();
 })();
-
-// click() {
-//   alert(`ты нажала на меня`);
-//   //stopPreparation()
-//  // const th = document.getElementById(`pos-${this.pos}`);
-//   let number = this.posNumber + 1;
-//   let indexOfPosLetter = letters.indexOf(this.posLetter);
-//   const rightLetter = letters[indexOfPosLetter + 1];
-//   const leftLetter = letters[indexOfPosLetter - 1];
-//   let th1 = document.getElementById(`pos-${leftLetter}-${number}`);
-//   let th2 = document.getElementById(`pos-${rightLetter}-${number}`);
-//   if (
-//     th1.hasChildNodes() ||
-//     th2.hasChildNodes() ||
-//     (th1.hasChildNodes() && th2.hasChildNodes())
-//   ) {
-//     alert(`nope`)
-//   }
-//   else {
-//     th1.style.backgroundColor = `yellow`;
-//     th2.style.backgroundColor = `yellow`;
-//   }
-
-//   //th.innerHTML = ``;
-
-//   // return this.pos
-// }
