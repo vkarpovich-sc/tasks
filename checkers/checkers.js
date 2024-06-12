532ea9832d18c58ea7dbdff99a11a1af6207ac24
vkarpovich-sc <vkarpovich@scand.com>
2024-06-12 13:17:40 +0300

    adding opportunity to hightlight possible moves

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -19,6 +19,8 @@ class Simple extends Checker {
 
   generateCheck() {
     const check = document.createElement(`div`);
+    //check.setAttribute(`id`, this.pos)
+    check.setAttribute(`id`, this.pos);
     check.style.cssText = `background-color: ${this.color};
     border: 2px solid gray;
     border-radius: 50px;
@@ -42,34 +44,46 @@ class Lady extends Checker {
 
 class Field {
   // создание поля при помощи класса клетки
-  constructor() {}
-  generateField() {
+
+  constructor() {
+    this.field = [];
+  }
+
+  generateField(Human, Bot) {
     //const div = document.getElementById(`container`);
     const table = document.getElementsByTagName(`table`)[0];
     const letters = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`];
-    const human = new Human();
-    const bot = new Bot();
     let checks = [];
+    this.field = [];
+
     for (let i = 0; i < 8; i++) {
       const tr = document.createElement(`tr`);
       checks[i] = [];
+      this.field[i] = [];
       for (let j = 0; j < 8; j++) {
         let cellColor = (i + j) % 2 === 0 ? "white" : "black";
-        const cell = new Cell(cellColor, `pos-${i + 1}-${letters[j]}`);
-        const cellElement = cell.generateCell();
+        const cell = new Cell(
+          cellColor,
+          `pos-${i + 1}-${letters[j]}`
+        ).generateCell();
+        // const cellElement = cell.generateCell();
         if (i == 3 || i == 4) {
-          tr.appendChild(cellElement);
+          this.field[i][j] = cell;
+          tr.appendChild(cell);
           continue;
         }
         if (i + 1 == 6 || i + 1 == 7 || i + 1 == 8) {
-          checks[i][j] = human.generateCheck(letters[j], i + 1);
+          checks[i][j] = Human.generateCheck(letters[j], i + 1);
+          this.field[i][j] = `pos-${letters[j]}-${i + 1}`;
         } else {
-          checks[i][j] = bot.generateCheck(letters[j], i + 1);
+          checks[i][j] = Bot.generateCheck(letters[j], i + 1);
+          this.field[i][j] = `pos-${letters[j]}-${i + 1}`;
         }
         if (cellColor == `black`) {
-          cellElement.append(checks[i][j]);
+          cell.append(checks[i][j]);
+          this.field[i][j] = checks[i][j];
         }
-        tr.append(cellElement);
+        tr.append(cell);
       }
       table.append(tr);
     }
@@ -82,14 +96,39 @@ class Player {
 
 class Human extends Player {
   constructor() {
-    super()
+    super();
     this.check = [];
+  }
+  checkNearest(field, n1, n2) {
+    console.log(field[n1][n2])
+    console.log(field[n1 - 1][n2 + 1])
+      if (field[n1 - 1][n2 + 1]!=`div` && field[n1 - 1][ n2 -1] != `div`) {
+        let rowEl1 = document.getElementById(`${field[n1 - 1][n2 + 1].id}`)
+        let rowEl2 = document.getElementById(`${field[n1 - 1][n2 - 1].id}`)
+        rowEl1.style.backgroundColor = `yellow`
+        rowEl2.style.backgroundColor = `yellow`
+      }
+  }
+  checkMove(field, target) {
+    const id = target.id;
+
+
+    let check = null
+    field.forEach((innerArray) => {
+      innerArray.forEach((el) => {
+        if (el.id == id){
+          check = el
+        // alert(`${field[field.indexOf(innerArray)][innerArray.indexOf(el)].id} + ${innerArray.indexOf(el)}`)
+          this.checkNearest(field,field.indexOf(innerArray),  innerArray.indexOf(el))
+        }
+      });
+    });
   }
   generateCheck(posL, posN) {
     const check = new Simple(posL, posN, `white`).generateCheck();
-    check.addEventListener(`click`, () => {
-      alert(`${posL} + ${posN}`);
-    });
+    // check.addEventListener(`click`, () => {
+    //   alert(`${posL} + ${posN}`);
+    // });
     this.check.push(check);
     return check;
   }
@@ -97,7 +136,7 @@ class Human extends Player {
 
 class Bot extends Player {
   constructor() {
-    super()
+    super();
     this.check = [];
   }
   generateCheck(posL, posN) {
@@ -126,6 +165,12 @@ class Cell {
     position: relative;`;
     return cell;
   }
+
+  changeColor(cell, cellColor) {
+    cellColor == `yellow`
+      ? (cell.style.backgroundColor = `blue`)
+      : cell.style.backgroundColor == `yellow`;
+  }
 }
 
 class Rules {
@@ -134,6 +179,19 @@ class Rules {
 
 class Game {
   constructor() {}
+
+  startGame() {
+    const field = new Field();
+    const human = new Human();
+    const bot = new Bot();
+    field.generateField(human, bot);
+    document.addEventListener("click", (event) => {
+      if (event.target.localName === `div`) {
+        human.checkMove(field.field, event.target);
+      }
+    });
+    console.log(field.field);
+  }
 }
 
 class Application {
@@ -141,6 +199,6 @@ class Application {
 }
 
 (function () {
-  const field = new Field();
-  field.generateField();
+  const game = new Game();
+  game.startGame();
 })();
