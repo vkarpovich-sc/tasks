d1e5b2c116b18bd84700a997e956b2e6656bbab9
vkarpovich-sc <vkarpovich@scand.com>
2024-07-04 10:48:50 +0300

    Update checkers.js

--- checkers/checkers.js
+++ checkers/checkers.js
@@ -18,13 +18,12 @@ class Simple extends Checker {
 
   generateCheck() {
     const check = document.createElement(`div`);
-
     check.setAttribute(`id`, this.pos);
     check.style.cssText = `background-color: ${this.color};
     border: 2px solid gray;
     border-radius: 50px;
-    width: 40px;
-    height:40px;
+    width: 60px;
+    height:60px;
     position: absolute;
     top: 50%;
     left: 50%;
@@ -86,13 +85,15 @@ class Player {
 }
 
 class Human extends Player {
-  constructor() {
+  constructor(color, direction) {
     super();
     this.check = [];
     this.clickedFigure = null;
+    this.color = color;
+    this.direction = direction;
   }
 
-  checkJump(field, n1, n2, cell, expression) {
+  checkJump(field, n1, n2, cell, expression, sign) {
     let i = 1;
     let lastFreeCell = null;
 
@@ -101,17 +102,23 @@ class Human extends Player {
 
       if (expression == 1) {
         isCellEmpty =
-          field[n1 - i][n2 - i] == undefined ||
-          field[n1 - i][n2 - i]?.localName === "div" ||
-          field[n1 - i][n2 - i] === null;
+          field[eval(`${n1}${sign}${i}`)][n2 - i] == undefined ||
+          field[eval(`${n1}${sign}${i}`)][n2 - i]?.localName === "div" ||
+          field[eval(`${n1}${sign}${i}`)][n2 - i] === null ||
+          field[eval(`${n1}${sign}${i}`)][n2 - i].style.backgroundColor ==
+            this.color;
 
-        if (isCellEmpty && cell.style.backgroundColor != "white") {
-          if (n1 - i < 0 || n2 - i < 0) {
+        if (isCellEmpty && cell.style.backgroundColor != this.color) {
+          if (eval(`${n1}${sign}${i}`) < 0 || n2 - i < 0) {
             return null;
           }
-          i++;
-          lastFreeCell = field[n1 - i][n2 - i];
 
+          i++;
+          if (n2 - i >= 0 && (n1 - i >= 0 || n1 + i < field.length)) {
+            lastFreeCell = field[eval(`${n1}${sign}${i}`)][n2 - i];
+          } else {
+            return null;
+          }
           if (
             isCellEmpty &&
             (lastFreeCell === undefined ||
@@ -119,16 +126,19 @@ class Human extends Player {
               lastFreeCell.localName != "th")
           ) {
             return null;
-          } else if (field[n1 - i][n2 - i].style.backgroundColor != "white") {
+          } else if (
+            field[eval(`${n1}${sign}${i}`)][n2 - i].style.backgroundColor !=
+            this.color
+          ) {
             checking();
           }
         } else {
-          const FREEPOS = field[n1 - i][n2 - i];
-          const lastPos = field[n1 - i - 1][n2 - i + 1];
+          const FREEPOS = field[eval(`${n1}${sign}${i}`)][n2 - i];
+          const lastPos = field[eval(`${n1}${sign}${i} - 1`)][n2 - i + 1];
 
           if (
             FREEPOS.localName != "div" &&
-            lastPos.style.backgroundColor != "white" &&
+            lastPos.style.backgroundColor != this.color &&
             i >= 2
           ) {
             lastFreeCell = FREEPOS;
@@ -138,16 +148,27 @@ class Human extends Player {
 
       if (expression == 2) {
         isCellEmpty =
-          field[n1 - i][n2 + i] == undefined ||
-          field[n1 - i][n2 + i]?.localName === "div" ||
-          field[n1 - i][n2 + i] === null;
-
-        if (isCellEmpty && cell.style.backgroundColor != "white") {
+          field[eval(`${n1}${sign}${i}`)][n2 + i] == undefined ||
+          field[eval(`${n1}${sign}${i}`)][n2 + i]?.localName === "div" ||
+          field[eval(`${n1}${sign}${i}`)][n2 + i] === null ||
+          field[eval(`${n1}${sign}${i}`)][n2 + i].style.backgroundColor ==
+            this.color;
+        if (isCellEmpty && cell.style.backgroundColor != this.color) {
           if (n2 + i >= field.length || n1 - i < 0) {
             return null;
+          } else {
+            i++;
+            if (
+              n2 + i < field.length &&
+              (n1 - i >= 0 || n1 + i < field.length)
+            ) {
+              lastFreeCell = field[eval(`${n1}${sign}${i}`)][n2 + i];
+
+              console.log(`lCell = ${lastFreeCell}`);
+            } else {
+              return null;
+            }
           }
-          i++;
-          lastFreeCell = field[n1 - i][n2 + i];
 
           if (
             isCellEmpty &&
@@ -156,23 +177,34 @@ class Human extends Player {
               lastFreeCell.localName != "th")
           ) {
             return null;
-          } else if (field[n1 - i][n2 + i].style.backgroundColor != "white") {
+          } else if (
+            field[eval(`${n1}${sign}${i}`)][n2 + i].style.backgroundColor !=
+            this.color
+          ) {
             checking();
           }
         } else {
-          const FREEPOS = field[n1 - i][n2 + i];
-          const lastPos = field[n1 - i - 1][n2 + i - 1];
+          const FREEPOS = field[eval(`${n1}${sign}${i}`)][n2 + i];
+          const lastPos = field[eval(`${n1}${sign}${i} - 1`)][n2 + i - 1];
 
           if (
             FREEPOS.localName != "div" &&
-            lastPos.style.backgroundColor != "white" &&
+            lastPos.style.backgroundColor != this.color &&
             i >= 2
           ) {
+            lastFreeCell = FREEPOS;
           }
         }
       }
     };
-    if (cell.style.backgroundColor === "black" && cell.localName === `div`) {
+    let oppositeColor = null;
+    this.color == `white`
+      ? (oppositeColor = `black`)
+      : (oppositeColor = `white`);
+    if (
+      cell.style.backgroundColor === oppositeColor &&
+      cell.localName === `div`
+    ) {
       checking();
     } else {
       return null;
@@ -180,25 +212,24 @@ class Human extends Player {
     i = 1;
     let lastPos = null;
     expression == 1
-      ? (lastPos = field[n1 - 1][n2 - 1])
-      : (lastPos = field[n1 - 1][n2 + 1]);
+      ? (lastPos = field[eval(`${n1}${sign}1`)][n2 - 1])
+      : (lastPos = field[eval(`${n1}${sign}1`)][n2 + 1]);
     if (
       lastFreeCell &&
       lastFreeCell.localName != `div` &&
       lastPos &&
-      lastPos.style.backgroundColor != `white`
+      lastPos.style.backgroundColor != this.color
     ) {
-      console.log(`lastFreecell - ${lastFreeCell.id}`);
       return lastFreeCell;
     } else {
       return null;
     }
   }
 
-  checkNearest(field, n1, n2) {
+  checkNearest(field, n1, n2, sign) {
     const cell = new Cell();
-    const cell1 = field[n1 - 1][n2 + 1];
-    const cell2 = field[n1 - 1][n2 - 1];
+    const cell1 = field[eval(`${n1}${sign}1`)][n2 + 1];
+    const cell2 = field[eval(`${n1}${sign}1`)][n2 - 1];
     const cell1IsEmpty = cell1 == undefined || cell1.localName === "div";
     const cell2IsEmpty = cell2 == undefined || cell2.localName === "div";
     const checkCondition = (
@@ -207,8 +238,8 @@ class Human extends Player {
       highlight,
       cellToHighlight = cellToCheck
     ) => {
-      const FREEPOS2 = this.checkJump(field, n1, n2, cellToCheck, 2);
-      const FREEPOS1 = this.checkJump(field, n1, n2, cellToCheck, 1);
+      const FREEPOS2 = this.checkJump(field, n1, n2, cellToCheck, 2, sign);
+      const FREEPOS1 = this.checkJump(field, n1, n2, cellToCheck, 1, sign);
       if (FREEPOS1 != null && FREEPOS2 != null) {
         cell.highlightMoves(FREEPOS1.id, FREEPOS2.id);
       } else if (FREEPOS1 != null && FREEPOS2 == null) {
@@ -251,16 +282,20 @@ class Human extends Player {
   }
 
   checkMove(field, target) {
+    //console.log(`id - ${target.id}`);
     const id = target.id;
-
+    let sign = null;
+    this.direction == 0 ? (sign = `-`) : (sign = `+`);
     field.forEach((innerArray) => {
       innerArray.forEach((el) => {
         if (el.id == id) {
           this.clickedFigure = el.id;
+
           this.checkNearest(
             field,
             field.indexOf(innerArray),
-            innerArray.indexOf(el)
+            innerArray.indexOf(el),
+            sign
           );
         }
       });
@@ -288,7 +323,7 @@ class Human extends Player {
     return field;
   }
   generateCheck(posL, posN) {
-    const check = new Simple(posL, posN, `white`).generateCheck();
+    const check = new Simple(posL, posN, this.color).generateCheck();
 
     this.check.push(check);
     return check;
@@ -296,37 +331,14 @@ class Human extends Player {
 }
 
 class Bot extends Player {
-  constructor() {
+  constructor(color) {
     super();
     this.check = [];
-  }
-
-  handleMove(field) {
-    const random = Math.floor(Math.random() * 12);
-
-    const check = this.check[random];
-
-    field.forEach((innerArray) => {
-      innerArray.forEach((pos) => {
-        if (
-          pos.localName != `div` &&
-          pos.style.backgroundColor == `rgb(23, 126, 211)`
-        ) {
-          check.id = pos.id;
-          const string = pos.id;
-          let g = string.split(`-`);
-
-          field[field.indexOf(innerArray)][innerArray.indexOf(pos)] =
-            this.generateCheck(g[2], g[1]);
-          pos.append(this.generateCheck(g[2], g[1]));
-        }
-      });
-    });
-    return field;
+    this.color = color;
   }
 
   generateCheck(posL, posN) {
-    const check = new Simple(posL, posN, `black`).generateCheck();
+    const check = new Simple(posL, posN, this.color).generateCheck();
     this.check.push(check);
     return check;
   }
@@ -376,49 +388,186 @@ class Game {
   constructor() {
     this.cellToClick1 = null;
     this.cellToClick2 = null;
+    this.turn = 0;
   }
 
+  chooseSide() {
+    return new Promise((resolve) => {
+      const OPTIONS = document.getElementById("options");
+      const CONTAINER = document.getElementById("container");
+      OPTIONS.style.cssText = `
+        position:absolute;
+        width: 100%;
+        height: 80%;`;
+      CONTAINER.style.display = "none";
+
+      const div1 = document.createElement("div");
+      const div2 = document.createElement("div");
+      const div3 = document.createElement("div");
+      const innerContainer = document.createElement(`div`);
+
+      const h2 = document.createElement("h2");
+      h2.innerText = "Выберите сторону: черные или белые";
+
+      div1.style.cssText = ` position: relative; width: 80%; height: 80%; border: 3px solid black; margin: auto; text-align: center;`;
+      div2.style.cssText = `background-color: white; width: 120px; height: 120px; border: 3px solid black; cursor: pointer; border-radius: 100px;`;
+      div3.style.cssText = `background-color: black; width: 120px; height: 120px; border: 3px solid black; cursor: pointer;border-radius: 100px;`;
+      innerContainer.style.cssText = `display: flex; flex-direction: row; justify-content: space-around;`;
+
+      div1.append(h2);
+      innerContainer.append(div2);
+      innerContainer.append(div3);
+      div1.append(innerContainer);
+      div1.addEventListener("click", (event) => {
+        if (event.target.localName == `div`) {
+          if (event.target.style.backgroundColor == `black`) {
+            OPTIONS.style.display = `none`;
+            CONTAINER.style.display = `grid`;
+            resolve({ color: `black` });
+          } else if (event.target.style.backgroundColor == `white`) {
+            OPTIONS.style.display = `none`;
+            CONTAINER.style.display = `grid`;
+            resolve({ color: `white` });
+          }
+        }
+      });
+      OPTIONS.append(div1);
+    });
+  }
+
+  chooseOpponent() {
+    // выбираем с кем играть: с ботом или игроком номер 2
+    return new Promise((resolve) => {});
+  }
+  isGameOver() {
+    if (Game.whiteChecks > Game.blackChecks) {
+      this.generateHistoryField(Game.blackChecks);
+      // this.generateHistoryField(`Победили белые`);
+    } else {
+      // this.generateHistoryField(`победили черные`);
+    }
+  }
+  handlePlayersMove(human, field, isOpp, cell) {
+    return new Promise((resolve, reject) => {
+      const handleClick = (event) => {
+        if (
+          event.target.localName === `div` &&
+          event.target.style.backgroundColor == human.color
+        ) {
+          cell.highlightMoves();
+          human.checkMove(field.field, event.target);
+        } else if (event.target.style.backgroundColor == `yellow`) {
+          this.generateHistoryField(`попали на клетку - ${event.target.id} `);
+
+          let a = document.getElementsByTagName(`div`);
+
+          a = Array.from(a);
+
+          a = a.find((el) => el.id == human.clickedFigure);
+          field.field = Game.removeCheck(
+            event.target.id,
+            field.field,
+            human.color,
+            isOpp
+          );
+          field.field = human.handleMove(a, field.field, event);
+
+          this.isGameOver();
+          document.removeEventListener(`click`, handleClick);
+          resolve(field.field);
+          console.log(field.field);
+        }
+      };
+      document.addEventListener("click", handleClick);
+
+      // resolve(field.field);
+    });
+  }
+
+  handleBotsMove(bot, field) {
+    field.field = bot.handleMove(field.field);
+    return field.field;
+  }
   generateHistoryField(move) {
     const div = document.getElementById(`container`);
     div.style.cssText = `display: grid;
      gap: 20px;
      grid-template-columns: 100% 10%;`;
     const innerDiv = document.getElementById(`innerDiv`);
+    innerDiv.style.cssText = `
+    max-height: 400px;
+    overflow-y:scroll;
+     border: 2px solid black;`;
     const p = document.createElement(`p`);
+    p.style.cssText = `border-bottom: 1px solid black;
+    padding:2px;`;
     p.innerHTML = move;
     innerDiv.append(p);
   }
-  static removeBlackCheck(id, field) {
-    const letters = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`];
-    let nums = id.split(`-`);
-    console.log(`id - ${id}`);
-    Game.blackChecks--;
-    const game = new Game();
-    //  game.generateHistoryField(nums[0]);
-    //  game.generateHistoryField(nums[1]);
-    //  game.generateHistoryField(nums[2]);
-    //  let k = nums.join(`-`);
-    //  game.generateHistoryField(k);
-    //  game.generateHistoryField(`index of nums[2] = ${letters.indexOf(nums[2])}`)
-    console.log(`nums[1] - ${parseInt(nums[1])}`);
-    const newPos1 = field[parseInt(nums[1])][letters.indexOf(nums[2]) + 1];
-    const newPos2 = field[parseInt(nums[1])][letters.indexOf(nums[2]) - 1];
-    console.log(`newPos - 1 - ${newPos1.id}, newPos2 - ${newPos2.id}`);
-    if (
-      newPos1.localName == `div` &&
-      newPos1.style.backgroundColor == `black`
-    ) {
-      game.generateHistoryField(`вы сбили шашку врага`);
-      game.generateHistoryField(`шашек врага осталось ${Game.blackChecks}`);
-      const cell = new Cell(`black`, newPos1.id);
-      field[parseInt(nums[1])][letters.indexOf(nums[2]) + 1] =
-        cell.generateCell();
 
-      document.getElementById(newPos1.id).replaceWith(` empty`);
+  static removeCheck(id, field, color, isOpp) {
+    const game = new Game();
+    const letters = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`];
+    const ids = id.split(`-`);
+    game.generateHistoryField(`id - ${id} && ids[1] = ${parseInt(ids[1])}`);
+    Game.blackChecks--;
+    const realPos = parseInt(ids[1]) - 1;
+    game.generateHistoryField(`realPos - ${realPos}`);
+    let newPos1 = null;
+    let newPos2 = null;
+    if (isOpp) {
+      newPos1 = field[parseInt(ids[1]) - 2][letters.indexOf(ids[2]) + 1];
+      newPos2 = field[parseInt(ids[1]) - 2][letters.indexOf(ids[2]) - 1];
+    } else {
+      newPos1 = field[parseInt(ids[1])][letters.indexOf(ids[2]) + 1];
+      newPos2 = field[parseInt(ids[1])][letters.indexOf(ids[2]) - 1];
     }
+
+    //console.log(`newPos1.id = ${newPos1.id}, newPos2.id = ${newPos2.id}`);
+
+    const replaceCellWithBlack = (cell) => {
+      //const game = new Game();
+      // game.generateHistoryField(`вы сбили шашку врага`);
+      //game.generateHistoryField(`шашек врага осталось ${Game.blackChecks}`);
+      const newCell = new Cell(`black`, cell.id);
+      game.generateHistoryField(`cell.id = ${cell.id}`);
+      const ths = Array.from(document.getElementsByTagName(`th`));
+      const targetCell = ths.find((el) => el.id == cell.id);
+      game.generateHistoryField(`id cell которую опустошаем-`, targetCell.id);
+      targetCell.innerHTML = ` `;
+      return newCell.generateCell();
+    };
+
+    let oppositeColor = null;
+    color == `white` ? (oppositeColor = `black`) : (oppositeColor = `white`);
+    console.log(`oppositeColor - ${oppositeColor}`);
+    if (
+      newPos1 &&
+      newPos1.localName == `div` &&
+      newPos1.style.backgroundColor == oppositeColor
+    ) {
+      isOpp
+        ? (field[parseInt(ids[1]) - 2][letters.indexOf(ids[2]) + 1] =
+            replaceCellWithBlack(newPos1))
+        : (field[parseInt(ids[1])][letters.indexOf(ids[2]) + 1] =
+            replaceCellWithBlack(newPos1));
+
+      return field;
+    } else if (
+      newPos2 &&
+      newPos2.localName == `div` &&
+      newPos2.style.backgroundColor == oppositeColor
+    ) {
+      isOpp
+        ? (field[parseInt(ids[1]) - 2][letters.indexOf(ids[2]) + 1] =
+            replaceCellWithBlack(newPos2))
+        : (field[parseInt(ids[1])][letters.indexOf(ids[2]) + 1] =
+            replaceCellWithBlack(newPos2));
+      return field;
+    }
+
     return field;
   }
-
   static removeWhiteCheck() {
     Game.whiteChecks--;
     const game = new Game();
@@ -426,33 +575,37 @@ class Game {
     game.generateHistoryField(`шашек врага осталось ${Game.whiteChecks}`);
   }
 
-  startGame() {
+  async startGame() {
+    const { color } = await this.chooseSide();
+    console.log(color);
+    let human = null;
+    let human2 = null;
+    let bot = null;
+    if (color == `white`) {
+      human = new Human(`white`, 0);
+      human2 = new Human(`black`, 1);
+      bot = new Bot(`black`);
+    } else {
+      human = new Human(`black`, 0);
+      human2 = new Human(`white`, 1);
+      bot = new Bot(`white`);
+    }
     const field = new Field();
-    const human = new Human();
-    const bot = new Bot();
-    field.generateField(human, bot);
+    const cell = new Cell();
+    field.generateField(human, human2);
+    // this.generateHistoryField(`игра началась`)
+    while (true) {
+      if (this.turn == 0) {
+        field.field = await this.handlePlayersMove(human, field, false, cell);
 
-    document.addEventListener("click", (event) => {
-      if (
-        event.target.localName === `div` &&
-        event.target.style.backgroundColor == `white`
-      ) {
-        human.checkMove(field.field, event.target);
-      } else if (event.target.style.backgroundColor == `yellow`) {
-        this.generateHistoryField(event.target.id);
+        this.turn = 1;
+      } else if (this.turn == 1) {
+        field.field = await this.handlePlayersMove(human2, field, true, cell);
 
-        let a = document.getElementsByTagName(`div`);
-
-        a = Array.from(a);
-
-        a = a.find((el) => el.id == human.clickedFigure);
-
-        field.field = human.handleMove(a, field.field, event);
-        Game.removeBlackCheck(event.target.id, field.field);
-        console.log(field.field);
+        this.turn = 0;
       }
-      field.field = bot.handleMove(field.field);
-    });
+      console.log(this.turn);
+    }
   }
 }
 
